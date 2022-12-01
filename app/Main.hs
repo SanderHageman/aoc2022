8ad0ba36d016db6bc73222a2e65de5ef47524f6d
Sander <s.a.t.hageman@students.uu.nl>
2022-12-01 23:09:13 +0100

    days

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,24 @@
 module Main where
-import Debug.Trace
-import Data.List
-import Text.Read (readMaybe)
+
+import           Data.Foldable (foldlM, foldrM)
+import           Data.List
+import           Debug.Trace
+import           Text.Read (readMaybe)
+import           Data.Functor
 
 main :: IO ()
-main = do
-  d1 <- day1
-  putStrLn $ "day1: " ++ d1
+main = days >>= putStrLn
+
+-- >>> days
+-- "day1: 66616 and 199172\n"
+days :: IO String
+days = do
+  let app = fst . foldl' f ("", 1)
+      f (r, i) x = (r ++ "day" ++ show i ++ ": " ++ x ++ "\n", i + 1)
+  sequence [day1] <&> app
 
 -- >>> day1
 -- "66616 and 199172"
-
 day1 :: IO String
 day1 = do
   input <- readFile "input/d1"
@@ -18,6 +26,7 @@ day1 = do
       top3 = (take 3 . reverse . sort) elves
       elves = map sum (splitInput input)
       splitInput = foldr (split . readMaybe) [[]] . lines
-        where split (Just i) (x:xs) = (i:x):xs
-              split _ xs = []:xs
+        where
+          split (Just i) (x:xs) = (i:x):xs
+          split _ xs = []:xs
   return $ show p1 ++ " and " ++ show p2
