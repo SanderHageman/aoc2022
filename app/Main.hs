78f99a396d7011bff35a053cb9827511f68164ac
Sander <s.a.t.hageman@students.uu.nl>
2022-12-02 14:31:52 +0100

    p2

--- app/Main.hs
+++ app/Main.hs
@@ -30,3 +30,30 @@ day1 = do
           split (Just i) (x:xs) = (i:x):xs
           split _ xs = []:xs
   return $ show p1 ++ " and " ++ show p2
+
+-- >>> day2
+-- "8933 and 11998"
+day2 :: IO String
+day2 = do
+  input <- readFile "input/d2"
+  let p1 = sum . map score  $ sets
+      p2 = sum . map score2 $ sets
+      sets = map (map head . words) $ lines input
+      score  [l,r] = (flip (-) 87 . fromEnum) r + wi l r
+      score2 [l,r] = score [l, case r of 'X' -> lose l
+                                         'Y' -> toEnum (fromEnum l + 23)
+                                         'Z' -> win l]
+        where lose 'A' = 'Z'
+              lose 'B' = 'X'
+              lose 'C' = 'Y'
+              win 'A' = 'Y'
+              win 'B' = 'Z'
+              win 'C' = 'X'
+      wi 'A' 'Y' = 6
+      wi 'A' 'Z' = 0
+      wi 'B' 'Z' = 6
+      wi 'B' 'X' = 0
+      wi 'C' 'X' = 6
+      wi 'C' 'Y' = 0
+      wi _ _ = 3
+  return $ show p1 ++ " and " ++ show p2
